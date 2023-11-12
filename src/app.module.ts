283a743d792f157fdc7095375df6d99dae893f80
tmddus2 <tmddus2__@naver.com>
2023-11-13 01:43:22 +0900

    Dokcerfile 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { BoardsModule } from './boards/boards.module';
-import { typeORMconfig } from './configs/typeorm.config';
-import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(typeORMconfig),
-    BoardsModule,
-    AuthModule
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
